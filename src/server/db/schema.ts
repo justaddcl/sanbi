249ca337e59e8a14028ac1b9380aecbeeadd5145
Yuji Nelson <k.yuji.nelson@gmail.com>
2024-06-16 22:32:32 +0100

    feat: adds user and song tables, key enum

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,9 +3,11 @@
 
 import { sql } from "drizzle-orm";
 import {
-  index,
+  integer,
+  pgEnum,
   pgTableCreator,
   serial,
+  text,
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
@@ -18,17 +20,58 @@ import {
  */
 export const createTable = pgTableCreator((name) => `sanbi_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt", { withTimezone: true }),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+// export const posts = createTable(
+//   "post",
+//   {
+//     id: serial("id").primaryKey(),
+//     name: varchar("name", { length: 256 }),
+//     createdAt: timestamp("created_at", { withTimezone: true })
+//       .default(sql`CURRENT_TIMESTAMP`)
+//       .notNull(),
+//     updatedAt: timestamp("updatedAt", { withTimezone: true }),
+//   },
+//   (example) => ({
+//     nameIndex: index("name_idx").on(example.name),
+//   })
+// );
+
+export const users = createTable("user", {
+  id: serial("id").primaryKey(),
+  firstName: varchar("firstName", { length: 256 }).notNull(),
+  lastName: varchar("lastName", { length: 256 }),
+  email: varchar("email", { length: 256 }).notNull().unique(),
+});
+
+export const songKeyEnum = pgEnum("song_key", [
+  "c",
+  "c_sharp",
+  "d_flat",
+  "d",
+  "d_sharp",
+  "e_flat",
+  "e",
+  "f",
+  "f_sharp",
+  "g_flat",
+  "g",
+  "g_sharp",
+  "a_flat",
+  "a",
+  "a_sharp",
+  "b_flat",
+  "b",
+]);
+
+export const songs = createTable("song", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  key: songKeyEnum("key"),
+  notes: text("notes"),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  createdBy: integer("user_id")
+    .references(() => users.id)
+    .notNull(),
+  updatedAt: timestamp("updatedAt", { withTimezone: true }),
+});
