ec1c782bd32e1d7fcc7bd8c01b0d477f07778c94
Yuji Nelson <k.yuji.nelson@gmail.com>
2024-06-25 22:05:50 +0100

    feat: integrates db with set details page

--- src/app/[organization]/sets/[setId]/page.tsx
+++ src/app/[organization]/sets/[setId]/page.tsx
@@ -1,20 +1,47 @@
+import { pluralize } from "@/lib/string";
 import { CardList, SetListCardBody, SongItem } from "@/modules/SetListCard";
+import { db } from "@/server/db";
+import { sets } from "@/server/db/schema";
 import { PageTitle } from "@components/PageTitle";
 import { Text } from "@components/Text";
 import { DotsThree } from "@phosphor-icons/react/dist/ssr";
+import { eq } from "drizzle-orm";
 import Link from "next/link";
 
 export default async function SetListPage({
-  params: _params,
+  params: params,
 }: {
   params: { setId: string };
 }) {
+  // FIXME: move query to db/queries
+  const setData = await db.query.sets.findFirst({
+    where: eq(sets.id, params.setId),
+    with: {
+      eventType: true,
+      sections: {
+        with: {
+          type: true,
+          songs: {
+            with: {
+              song: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  console.log("🚀 ~ setData:", setData);
+  const songCount =
+    setData?.sections.reduce(
+      (total, section) => total + section.songs.length,
+      0,
+    ) ?? 0;
   return (
     <div className="flex min-w-full max-w-xs flex-col justify-center gap-6">
       <PageTitle
-        title="August 07"
-        subtitle="Discipleship Community"
-        details="8 songs"
+        title={setData!.date}
+        subtitle={setData!.eventType.event}
+        details={`${songCount} ${pluralize(songCount, { singular: "song", plural: "songs" })}`}
       />
       <section className="flex justify-between gap-2">
         <button className="w-full rounded border border-slate-300 px-3 text-xs font-semibold text-slate-700">
@@ -24,84 +51,61 @@ export default async function SetListPage({
           <DotsThree className="text-slate-900" size={12} />
         </button>
       </section>
-      <CardList gap="gap-6">
-        <div className="flex flex-col gap-4 rounded border border-slate-200 p-4 shadow">
-          <header className="flex flex-col gap-2">
-            <div className="flex justify-between">
-              <Text asElement="h3" style="header-medium-semibold">
-                Worship
-              </Text>
-              <button className="flex h-6 w-6 place-content-center rounded border border-slate-300 p-[6px]">
-                <DotsThree className="text-slate-900" size={12} />
-              </button>
+      {setData?.sections && setData.sections.length > 0 && (
+        <CardList gap="gap-6">
+          {setData.sections.map((section) => (
+            // FIXME: refactor into reusable component
+            <div
+              key={section.id}
+              className="flex flex-col gap-4 rounded border border-slate-200 p-4 shadow"
+            >
+              <header className="flex flex-col gap-2">
+                <div className="flex justify-between">
+                  <Text asElement="h3" style="header-medium-semibold">
+                    {section.type.section}
+                  </Text>
+                  <button className="flex h-6 w-6 place-content-center rounded border border-slate-300 p-[6px]">
+                    <DotsThree className="text-slate-900" size={12} />
+                  </button>
+                </div>
+                <hr className="bg-slate-100" />
+              </header>
+              <SetListCardBody>
+                {section.songs && section.songs.length > 0 && (
+                  <div className="flex flex-col gap-3">
+                    {section.songs.map((setSectionSong) => {
+                      let indexStart = 1;
+
+                      for (
+                        let sectionPosition = 0;
+                        sectionPosition < section.position;
+                        sectionPosition++
+                      ) {
+                        indexStart +=
+                          setData.sections[sectionPosition]!.songs.length;
+                      }
+
+                      return (
+                        <Link
+                          key={setSectionSong.songId}
+                          href={`../songs/${setSectionSong.songId}`}
+                        >
+                          <SongItem
+                            index={indexStart + setSectionSong.position}
+                            songKey={setSectionSong.song!.key}
+                            name={setSectionSong.song!.name}
+                            notes={setSectionSong.song!.notes}
+                          />
+                        </Link>
+                      );
+                    })}
+                  </div>
+                )}
+              </SetListCardBody>
             </div>
-            <hr className="bg-slate-100" />
-          </header>
-          <SetListCardBody>
-            <div className="flex flex-col gap-3">
-              <Link href={`../songs/demo`}>
-                <SongItem
-                  index={1}
-                  songKey="B"
-                  name="In My Place"
-                  notes="Play in the key of B to make it easier for the backup vocalist to harmonize with."
-                />
-              </Link>
-              <SongItem index={2} songKey="A" name="Such An Awesome God" />
-              <SongItem
-                index={3}
-                songKey="G"
-                name="I Love You Lord / What A Beautiful Name (mash up)"
-                notes="Song is best with only vocals, guitar, and keys. Feels powerful with only vocals on the last chorus."
-              />
-              <SongItem index={4} songKey="A" name="God Over Everything" />
-            </div>
-          </SetListCardBody>
-        </div>
-        <div className="flex flex-col gap-4 rounded border border-slate-200 p-4 shadow">
-          <header className="flex flex-col gap-2">
-            <div className="flex justify-between">
-              <Text asElement="h3" style="header-medium-semibold">
-                Lord&apos;s Supper
-              </Text>
-              <button className="flex h-6 w-6 place-content-center rounded border border-slate-300 p-[6px]">
-                <DotsThree className="text-slate-900" size={12} />
-              </button>
-            </div>
-            <hr className="bg-slate-100" />
-          </header>
-          <SetListCardBody>
-            <div className="flex flex-col gap-3">
-              <SongItem index={5} songKey="B" name="Son Of Suffering" />
-            </div>
-          </SetListCardBody>
-        </div>
-        <div className="flex flex-col gap-4 rounded border border-slate-200 p-4 shadow">
-          <header className="flex flex-col gap-2">
-            <div className="flex justify-between">
-              <Text asElement="h3" style="header-medium-semibold">
-                Prayer
-              </Text>
-              <button className="flex h-6 w-6 place-content-center rounded border border-slate-300 p-[6px]">
-                <DotsThree className="text-slate-900" size={12} />
-              </button>
-            </div>
-            <hr className="bg-slate-100" />
-          </header>
-          <SetListCardBody>
-            <div className="flex flex-col gap-3">
-              <SongItem index={6} songKey="B" name="Draw Me Close To You" />
-              <SongItem index={7} songKey="A" name="Romans 2:4" />
-              <SongItem
-                index={8}
-                songKey="G"
-                name="Only Jesus"
-                notes="Skip the tag if not playing with full band."
-              />
-            </div>
-          </SetListCardBody>
-        </div>
-      </CardList>
+          ))}
+        </CardList>
+      )}
     </div>
   );
 }
