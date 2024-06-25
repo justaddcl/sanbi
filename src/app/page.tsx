92d7d52fce424c2dbf1448337003e4ea476d87f9
Yuji Nelson <k.yuji.nelson@gmail.com>
2024-06-25 21:56:03 +0100

    feat: updates home page to include dynamic link to Stoneway org

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import { db } from "@/server/db";
+import { organizations } from "@/server/db/schema";
+import { eq } from "drizzle-orm";
+import Link from "next/link";
+
 export default async function Home() {
+  // FIXME: move query to db/queries
+  const [stonewayOrg] = await db
+    .select()
+    .from(organizations)
+    .where(eq(organizations.name, "Stoneway"));
+
+  if (!stonewayOrg) {
+    throw new Error("No organizations.");
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      賛美 // Sanbi
+      <Link href={`${stonewayOrg.id}/`}>賛美 // Sanbi</Link>
     </main>
   );
 }
