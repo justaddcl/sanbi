a1e71b8cf253bc6403143602654bc71e0227f4a1
Yuji Nelson <k.yuji.nelson@gmail.com>
2024-06-26 22:32:55 +0100

    fix: adds POSTGRES_URL to runtimeEnv config

--- src/env.js
+++ src/env.js
@@ -29,6 +29,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    POSTGRES_URL: process.env.POSTGRES_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
