09d5501105c30056dc37db14e0b412bfb9e1e269
Yuji Nelson <k.yuji.nelson@gmail.com>
2024-06-28 23:07:09 +0100

    feat: removes local db migration

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,7 +2,6 @@ import {
   drizzle as LocalDrizzle,
   type PostgresJsDatabase,
 } from "drizzle-orm/postgres-js";
-import { migrate as LocalMigrate } from "drizzle-orm/postgres-js/migrator";
 import {
   drizzle as VercelDrizzle,
   type VercelPgDatabase,
@@ -21,18 +20,13 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const MIGRATIONS_DIR = "./src/server/db/migrations";
-
 let db: PostgresJsDatabase<typeof schema> | VercelPgDatabase<typeof schema>;
 
 const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
 if (env.NODE_ENV !== "production") {
   globalForDb.conn = conn;
-  const migrationClient = postgres(env.DATABASE_URL, { max: 1 });
   const queryClient = postgres(env.DATABASE_URL);
   db = LocalDrizzle(queryClient, { schema });
-  await LocalMigrate(db, { migrationsFolder: MIGRATIONS_DIR });
-  await migrationClient.end();
 } else {
   db = VercelDrizzle(sql, { schema });
 }
