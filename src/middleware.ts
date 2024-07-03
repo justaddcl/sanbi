23ae9301a9cf772ae4e86fd538f7ae50959c6748
Yuji Nelson <k.yuji.nelson@gmail.com>
2024-07-03 20:26:34 +0100

    feat: whitelists example sentry pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,8 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/monitoring(.*)",
+  "/sentry-example-page(.*)",
+  "/api/sentry-example-api(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
