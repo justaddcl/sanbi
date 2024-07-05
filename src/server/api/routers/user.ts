859fcde7b64d7253c750c2de25260bc1013def8f
Yuji Nelson <k.yuji.nelson@gmail.com>
2024-07-05 21:02:11 +0100

    feat: adds api to create sanbi user for authed user

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -6,6 +6,10 @@ import {
   publicProcedure,
 } from "@server/api/trpc";
 import { users } from "@server/db/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { type NewUser } from "@/lib/types";
+import { eq } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
 
 export const userRouter = createTRPCRouter({
   hello: authedProcedure.query(({ ctx }) => {
@@ -15,6 +19,68 @@ export const userRouter = createTRPCRouter({
   }),
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.query.users.findMany();
+  }),
+  createMe: authedProcedure.mutation(async ({ ctx }) => {
+    const { userId } = ctx.auth;
+
+    if (!userId) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+
+    const matchingUser = await ctx.db.query.users.findFirst({
+      where: eq(users.id, userId),
+    });
+
+    if (matchingUser) {
+      throw new TRPCError({
+        code: "CONFLICT",
+        message: `User ${userId} already exists`,
+      });
+    }
+
+    const user = await currentUser();
+
+    if (!user) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: `User not found`,
+      });
+    }
+
+    const userEmail = user.primaryEmailAddress?.emailAddress;
+    if (!userEmail) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "User must have a primary email address",
+      });
+    }
+
+    if (!user.firstName) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "User must have a first name",
+      });
+    }
+
+    if (!user.lastName) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "User must have a last name",
+      });
+    }
+
+    const newUser: NewUser = {
+      id: userId,
+      email: userEmail,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    };
+
+    return ctx.db
+      .insert(users)
+      .values(newUser)
+      .onConflictDoNothing({ target: users.id })
+      .returning();
   }),
 
   // create: publicProcedure
