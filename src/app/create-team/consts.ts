a92857e796c6c9f05016ec7b218a6967c40979f4
Yuji Nelson <k.yuji.nelson@gmail.com>
2024-07-05 21:03:36 +0100

    feat: updates create-team page to create user post-sign-up

---
+++ src/app/create-team/consts.ts
@@ -0,0 +1,1 @@
+export const NEW_USER_SIGN_UP_KEY = "newUserSignUp";
--- src/app/create-team/page.tsx
+++ src/app/create-team/page.tsx
@@ -1,12 +1,28 @@
 import { api } from "@/trpc/server";
+import { TRPCError } from "@trpc/server";
+import { NEW_USER_SIGN_UP_KEY } from "@app/create-team/consts";
 
-export default async function CreateTeamPage() {
-  const greeting = await api.user.hello({ text: "Hellomst" });
-
-  return (
-    <p>
-      Create a team. Or don&apos;t. I don&apos;t control you. Also, your mom
-      said {greeting ? greeting.greeting : "nothing"}
-    </p>
+export default async function CreateTeamPage({
+  searchParams,
+}: {
+  searchParams: Record<string, string | string[] | undefined>;
+}) {
+  const shouldCreateNewUser = Object.keys(searchParams).includes(
+    NEW_USER_SIGN_UP_KEY as string, // type assertion here since ESLint removes `string` typing on the const
   );
+
+  if (shouldCreateNewUser) {
+    try {
+      const newUser = await api.user.createMe();
+      console.log("User created:", newUser);
+    } catch (createUserError) {
+      if (createUserError instanceof TRPCError) {
+        if (createUserError.code === "CONFLICT") {
+          console.info(createUserError.message);
+        }
+      }
+    }
+  }
+
+  return <p>Create a team</p>;
 }
