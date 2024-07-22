a07307f252ebb59236a3cca3c078d38d221d1189
Yuji Nelson <k.yuji.nelson@gmail.com>
2024-07-22 23:03:30 +0100

    fix: fixes lint errors

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,6 @@
 import { api } from "@/trpc/server";
 
-export async function GET(request: Request) {
+export async function GET(_request: Request) {
   const users = await api.user.getAll();
   return Response.json({ users });
 }
