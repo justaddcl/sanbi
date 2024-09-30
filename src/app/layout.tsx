588a202578a1dc0ab8f11b0bdb807a35d4d646d7
Yuji Nelson <k.yuji.nelson@gmail.com>
2024-09-30 15:59:31 +0100

    feat: adds viewport meta to layout to stop unwanted zooming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${poppins.variable}`}>
+        <head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
+        </head>
         <body>
           <TRPCReactProvider>
             <SanbiStoreProvider>
