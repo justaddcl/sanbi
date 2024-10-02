c5285452bb3b496b1ab4918d3c998b35d259de8a
Yuji Nelson <k.yuji.nelson@gmail.com>
2024-10-02 21:44:51 +0100

    feat: deprecates color prop for Text component

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,15 @@ const config = {
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint", "drizzle"],
+  plugins: ["@typescript-eslint", "drizzle", "deprecation"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
+    // warn when using code marked as @deprecated
+    "deprecation/deprecation": "warn",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
