d1663bf71eb576805b94b68dd4548305b58f8e4d
Yuji Nelson <k.yuji.nelson@gmail.com>
2025-01-20 22:02:09 +0000

    Updates CardList component to use VStack component

--- src/modules/SetListCard/components/CardList/CardList.tsx
+++ src/modules/SetListCard/components/CardList/CardList.tsx
@@ -1,3 +1,5 @@
+import { VStack } from "@components/VStack";
+
 export type CardListProps = {
   /** spacing between cards - uses TailwindCSS values - https://tailwindcss.com/docs/gap  */
   gap?: string;
@@ -7,5 +9,5 @@ export const CardList: React.FC<React.PropsWithChildren<CardListProps>> = ({
   gap = "gap-4",
   children,
 }) => {
-  return <div className={`flex flex-col ${gap}`}>{children}</div>;
+  return <VStack className={`flex flex-col ${gap}`}>{children}</VStack>;
 };
