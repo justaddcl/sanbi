e94182552f515f802a013e24e1d1401d49164b48
Yuji Nelson <k.yuji.nelson@gmail.com>
2025-01-22 21:20:33 +0000

    Updates form fields to match required label in UI and removes unnecessary console.log

--- src/modules/sets/components/CreateSongForm/CreateSongForm.tsx
+++ src/modules/sets/components/CreateSongForm/CreateSongForm.tsx
@@ -29,14 +29,13 @@ import { formatSongKey } from "@/lib/string/formatSongKey";
 const createSongFormSchema = insertSongSchema
   .pick({
     name: true,
-    preferredKey: true,
   })
   .extend({
     /**
      * by default, zod thinks preferred key and notes are nullable which causes type issues.
      * these fields have been manually re-defined to omit the nullable
      */
-    preferredKey: z.enum(songKeys).optional(), // TODO: confirm if this should be optional
+    preferredKey: z.enum(songKeys),
     notes: z.string().optional(),
   });
 
@@ -72,7 +71,6 @@ export const CreateSongForm: React.FC<CreateSongFormProps> = ({ onSubmit }) => {
     });
 
   const handleCreateSongSubmit = async (formValues: CreateSongFormFields) => {
-    console.log("🚀 ~ handleCreateSongSubmit ~ formValues:", formValues);
     const { name, preferredKey, notes } = formValues;
 
     // attempt to create song
