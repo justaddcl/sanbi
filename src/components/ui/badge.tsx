771cec3c4e0d3d5b1cfb45af1e14d124a08fdb12
Yuji Nelson <k.yuji.nelson@gmail.com>
2025-04-21 00:40:10 +0100

    Extends badge component to allow dimissing

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
+import { X } from "@phosphor-icons/react/dist/ssr/X";
 
 const badgeVariants = cva(
   "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
@@ -24,13 +25,38 @@ const badgeVariants = cva(
   },
 );
 
-export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
+export type BadgeProps = React.HTMLAttributes<HTMLDivElement> &
+  VariantProps<typeof badgeVariants> & {
+    dismissable?: boolean;
+    onClose?: () => void;
+    onClosePending?: boolean;
+  };
 
-function Badge({ className, variant, ...props }: BadgeProps) {
+function Badge({
+  className,
+  variant,
+  dismissable,
+  onClose,
+  onClosePending,
+  children,
+  ...props
+}: BadgeProps) {
   return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+    <div className={cn(badgeVariants({ variant }), className)} {...props}>
+      {children}
+      {dismissable && onClose && (
+        <button
+          onClick={(closeEvent) => {
+            closeEvent.stopPropagation();
+            onClose();
+          }}
+          disabled={onClosePending}
+          className="ml-2 rounded-full outline-none ring-offset-background focus:ring-2 focus:ring-ring focus:ring-offset-2"
+        >
+          <X className="h-3.5 w-3.5 text-muted-foreground transition-colors hover:text-foreground" />
+        </button>
+      )}
+    </div>
   );
 }
 
