6aeb63b3895ab25b78ca64ac3f6ff7cab01d7e7a
Yuji Nelson <k.yuji.nelson@gmail.com>
2025-07-05 14:28:11 +0100

    Adds date tooltip for selected set card

--- src/modules/songs/forms/AddSongToSet/components/SelectedSetCard/SelectedSetCard.tsx
+++ src/modules/songs/forms/AddSongToSet/components/SelectedSetCard/SelectedSetCard.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { differenceInCalendarWeeks } from "date-fns";
 
 import { Badge } from "@components/ui/badge";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@components/ui/tooltip";
 import { HStack } from "@components/HStack";
 import { Text } from "@components/Text";
 import { VStack } from "@components/VStack";
-import { formatFriendlyDate } from "@lib/date";
+import {
+  formatDate,
+  formatFriendlyDate,
+  FRIENDLY_FORMAT_CALENDAR_WEEK_DIFFERENCE_THRESHOLD,
+} from "@lib/date";
 import { pluralize } from "@lib/string";
 import { type RouterOutputs } from "@/trpc/react";
 
@@ -31,12 +41,28 @@ export const SelectedSetCard: React.FC<SelectedSetCardProps> = ({
     plural: countShown === "songs" ? "songs" : "sections",
   };
 
+  const shouldShowDateTooltip =
+    differenceInCalendarWeeks(set.date, new Date(), {
+      weekStartsOn: 0,
+    }) < FRIENDLY_FORMAT_CALENDAR_WEEK_DIFFERENCE_THRESHOLD;
+
   return (
     <HStack className="items-center justify-between rounded-lg border border-slate-200 p-3">
       <VStack>
-        <Text className="font-medium md:text-lg">
-          {formatFriendlyDate(set.date)}
-        </Text>
+        {shouldShowDateTooltip ? (
+          <Tooltip>
+            <TooltipTrigger>
+              <Text className="font-medium md:text-lg">
+                {formatFriendlyDate(set.date)}
+              </Text>
+            </TooltipTrigger>
+            <TooltipContent>{formatDate(set.date)}</TooltipContent>
+          </Tooltip>
+        ) : (
+          <Text className="font-medium md:text-lg">
+            {formatFriendlyDate(set.date)}
+          </Text>
+        )}
         <Text className="text-sm text-slate-500">{set.eventType.name}</Text>
       </VStack>
       <Badge variant="secondary">
