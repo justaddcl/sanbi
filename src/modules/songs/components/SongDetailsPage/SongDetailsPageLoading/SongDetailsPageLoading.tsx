f11eb0db90e3ed1194040185138c7fcc8d0ca222
Yuji Nelson <k.yuji.nelson@gmail.com>
2026-01-06 21:01:10 +0000

    Updates song detais page skeleton to mirror hydrated page

--- src/modules/songs/components/SongDetailsPage/SongDetailsPageLoading/SongDetailsPageLoading.tsx
+++ src/modules/songs/components/SongDetailsPage/SongDetailsPageLoading/SongDetailsPageLoading.tsx
@@ -1,13 +1,28 @@
+import { DotsThree, Heart, Plus } from "@phosphor-icons/react/dist/ssr";
+
+import { Button } from "@components/ui/button";
+import { Skeleton } from "@components/ui/skeleton";
 import { Card } from "@components/Card/Card";
 import { HStack } from "@components/HStack";
-import { PageContentContainer } from "@components/PageContentContainer";
-import { Skeleton } from "@components/ui/skeleton";
 import { VStack } from "@components/VStack";
 import { SongDetailsItem } from "@modules/songs/components";
 
 export const SongDetailsPageLoading: React.FC = () => (
-  <PageContentContainer>
-    <Skeleton className="h-8 w-full" />
+  <>
+    <HStack className="gap-4">
+      <Skeleton className="h-10 w-full" />
+      <HStack className="items-start gap-2">
+        <Button>
+          <Plus /> Add to a set
+        </Button>
+        <Button variant="outline" disabled size="sm">
+          <Heart />
+        </Button>
+        <Button variant={"outline"} size="sm">
+          <DotsThree className="text-slate-900" size={16} />
+        </Button>
+      </HStack>
+    </HStack>
     <Card title="Song details" collapsible>
       <VStack as="dl" className="gap-4 md:gap-6">
         <SongDetailsItem icon="MusicNoteSimple" label="Preferred Key">
@@ -37,17 +52,21 @@ export const SongDetailsPageLoading: React.FC = () => (
         </SongDetailsItem>
       </VStack>
     </Card>
-    <HStack as="section" className="justify-between gap-2">
-      <Skeleton className="w-full" />
-      <Skeleton className="size-6" />
-      <Skeleton className="size-6" />
-    </HStack>
-    <Card title="Resources" collapsible>
-      <div className="grid grid-cols-[repeat(auto-fill,_124px)] grid-rows-[repeat(auto-fill,_92px)] gap-2">
-        <Skeleton className="h-full w-full" />
-        <Skeleton className="h-full w-full" />
-        <Skeleton className="h-full w-full" />
-      </div>
+    <Card
+      title="Resources"
+      collapsible
+      button={
+        <Button size="sm" variant="ghost" disabled>
+          <Plus className="text-slate-900" size={16} />
+          <span className="hidden sm:inline">Add resource</span>
+        </Button>
+      }
+    >
+      <ul className="grid gap-3 md:grid-cols-2">
+        <Skeleton className="h-16" />
+        <Skeleton className="h-16" />
+        <Skeleton className="h-16" />
+      </ul>
     </Card>
     <Card title="Play history" collapsible>
       <div className="grid grid-cols-[16px_1fr] gap-y-4">
@@ -78,5 +97,5 @@ export const SongDetailsPageLoading: React.FC = () => (
         </VStack>
       </div>
     </Card>
-  </PageContentContainer>
+  </>
 );
