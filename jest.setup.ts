ff04f74b7abc123f1008322201823f3ef8ee37ca
Yuji Nelson <k.yuji.nelson@gmail.com>
2026-06-06 23:35:10 +0100

    Keep Jest 30 compatible with Next ESM transforms

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,23 @@
+import { TransformStream } from "node:stream/web";
+
 import "@testing-library/jest-dom";
+
+if (!globalThis.TransformStream) {
+  Object.defineProperty(globalThis, "TransformStream", {
+    configurable: true,
+    value: TransformStream,
+    writable: true,
+  });
+}
+
+if (!globalThis.fetch) {
+  const fetchMock = jest.fn(() =>
+    Promise.reject(new Error("Unexpected fetch call in test")),
+  );
+
+  Object.defineProperty(globalThis, "fetch", {
+    configurable: true,
+    value: fetchMock,
+    writable: true,
+  });
+}
