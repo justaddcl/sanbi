2d26c41043e8730973994dfab8c03abb4a35cecc
Yuji Nelson <k.yuji.nelson@gmail.com>
2026-06-10 22:27:53 +0100

    Load Poppins in Storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Poppins } from "next/font/google";
 import type { Decorator, Preview } from "@storybook/nextjs-vite";
 
 import { Toaster } from "@components/ui/sonner";
@@ -6,14 +7,22 @@ import { TooltipProvider } from "@components/ui/tooltip";
 
 import "../src/styles/globals.css";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-poppins",
+});
+
 const LightModeFrame: React.FC<React.PropsWithChildren> = ({ children }) => {
   useEffect(() => {
     document.documentElement.classList.remove("dark");
     document.documentElement.style.colorScheme = "light";
-    document.documentElement.style.setProperty(
-      "--font-poppins",
-      "Poppins, ui-sans-serif, system-ui, sans-serif",
-    );
+    document.documentElement.classList.add(poppins.variable);
+
+    return () => {
+      document.documentElement.classList.remove(poppins.variable);
+    };
   }, []);
 
   return (
